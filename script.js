634b52dff27d1c5c50b360279cf1c1e79a0e46ba
deanviper <153517604+deanviper@users.noreply.github.com>
2023-12-11 21:47:06 +0800

    Changed Style and Animation

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ function toggleRibbon() {
 function openLetter(friendName) {
     var letterContent = getLetterContent(friendName);
 
+    document.getElementById('friend-box-container').style.display = 'none';
     document.getElementById('letter-container').style.display = 'block';
     document.getElementById('letter-container').innerHTML = letterContent;
+}
+
+function exitLetter() {
+    document.getElementById('letter-container').style.display = 'none';
+    document.getElementById('friend-box-container').style.display = 'block';
 }
 
 function getLetterContent(friendName) {
@@ -15,5 +21,6 @@ function getLetterContent(friendName) {
                 <h2>${friendName}'s Letter</h2>
                 <img src="${friendName}.jpg" alt="${friendName}'s Letter">
                 <p>This is a letter from ${friendName}...</p>
+                <div class="exit-button" onclick="exitLetter()">Exit</div>
             </div>`;
 }
