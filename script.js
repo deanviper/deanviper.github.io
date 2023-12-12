7a5e27fd6098883d28ad2c8ca0216d36d326290a
deanviper <153517604+deanviper@users.noreply.github.com>
2023-12-13 03:04:27 +0800

    crazx

--- script.js
+++ script.js
@@ -79,9 +79,7 @@ function createFadedWords(words) {
 var words = [
     "palangga", "tapsecret", "doc", "koomi", "supernatural", "ay?!", "greys anatomy", "8-ball",
     "green apple", "damit sa closet", "crunch chicken", "mayon skyline", "motorcycle", "javier",
-    "olivia", "green", "timong kong nag-iisa", "ucc", "gin", "marriot", "lorem", "ipsum", "dolor",
-    "sit", "amet", "consectetur", "adipiscing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt",
-    "ut", "labore", "et", "dolore", "magna", "aliqua"
+    "olivia", "green", "timong kong nag-iisa", "ucc", "gin", "marriot"
 ];
 
 createFadedWords(words);
